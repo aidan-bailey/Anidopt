96db54f81fed56c874d6fe5083acccb4f2594052
Aidan Bailey <aidan.j.bailey@outlook.com>
2023-03-25 13:24:04 +0200

    Add AnimalType to Breed

--- Anidopt/Models/Breed.cs
+++ Anidopt/Models/Breed.cs
@@ -8,4 +8,6 @@ public class Breed
     [StringLength(32, MinimumLength = 1)]
     [Required]
     public string? Name { get; set; }
+    public virtual AnimalType? AnimalType { get; set; }
+    public int AnimalTypeId { get; set; }
 }
