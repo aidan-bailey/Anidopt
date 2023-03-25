f36e88f444b63c0584e9ba91b0d021477a158ddf
Aidan Bailey <aidan.j.bailey@outlook.com>
2023-03-25 09:23:03 +0200

    Add validation
    - `Required` on Name and Age.
    - `StringLength` (max 32, min 1) on Name

--- Anidopt/Models/Animal.cs
+++ Anidopt/Models/Animal.cs
@@ -6,7 +6,10 @@ public class Animal
 {
 
     public int Id { get; set; }
+    [StringLength(32, MinimumLength = 1)]
+    [Required]
     public string? Name { get; set; }
+    [Required]
     public int Age { get; set; }
 
 }
