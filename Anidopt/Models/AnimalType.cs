6fcae299545c5c3b332c5238ef6cea0c622ec2e4
Aidan Bailey <aidan.j.bailey@outlook.com>
2023-03-25 09:25:00 +0200

    Add AnimalType.cs

---
+++ Anidopt/Models/AnimalType.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Anidopt.Models;
+
+public class AnimalType
+{
+    public int Id { get; set; }
+    [StringLength(32, MinimumLength = 1)]
+    [Required]
+    public string Name { get; set; }
+}
