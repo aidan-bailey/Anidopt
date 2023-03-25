459948d87ebb62ab07445ee3f910eb18c4795ea2
Aidan Bailey <aidan.j.bailey@outlook.com>
2023-03-25 08:43:43 +0200

    Add SeedData.cs

---
+++ Anidopt/Models/SeedData.cs
@@ -0,0 +1,30 @@
+﻿using Anidopt.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Anidopt.Models;
+
+public static class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new AnidoptContext(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<AnidoptContext>>()))
+        {
+            // Look for any movies.
+            if (context.Animal.Any())
+            {
+                return;   // DB has been seeded
+            }
+            context.Animal.AddRange(
+                new Animal
+                {
+                    Name = "Dimitri",
+                    Age = 1,
+                    Id = 0
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
