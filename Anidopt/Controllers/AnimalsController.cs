6db33fdeeb0d88673b66ab816eb3fdee70859a7a
Aidan Bailey <aidan.j.bailey@outlook.com>
2023-03-25 11:01:53 +0200

    Cut unused Id bind

--- Anidopt/Controllers/AnimalsController.cs
+++ Anidopt/Controllers/AnimalsController.cs
@@ -60,7 +60,7 @@ namespace Anidopt.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,Age,AnimalTypeId")] Animal animal)
+        public async Task<IActionResult> Create([Bind("Name,Age,AnimalTypeId")] Animal animal)
         {
             if (ModelState.IsValid)
             {
