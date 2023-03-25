24a345bcb2b84d6b33e560ecfa86f8cb8e9e6f37
Aidan Bailey <aidan.j.bailey@outlook.com>
2023-03-25 12:23:51 +0200

    Add Organisation.cs

---
+++ Anidopt/Models/Organisation.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Anidopt.Models;
+
+#pragma warning disable CS8618
+
+public class Organisation
+{
+    public int Id;
+    [Required]
+    [StringLength(32, MinimumLength = 1)]
+    public string Name;
+    public List<Animal> Animals;
+    public List<int> AnimalIds;
+}
