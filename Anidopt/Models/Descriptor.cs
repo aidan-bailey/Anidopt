461d19df48f5619651beca9fe6581c8c71b9ecd1
Aidan Bailey <aidan.j.bailey@outlook.com>
2023-03-26 15:59:12 +0200

    Add Descriptor model

---
+++ Anidopt/Models/Descriptor.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Anidopt.Models;
+
+public class Descriptor
+{
+    public int Id { get; set; }
+    public virtual DescriptorType? DescriptorType { get; set; }
+    [Required]
+    public int DescriptorTypeId { get; set; }
+    [Required]
+    public string? Name { get; set; }
+}
