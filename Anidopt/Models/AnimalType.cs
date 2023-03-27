b820c4e37af6b9627604dc9c298fcfa8b0ca9af8
Aidan Bailey <aidan.j.bailey@outlook.com>
2023-03-27 20:17:09 +0200

    Add AnimalType constraints

--- Anidopt/Models/AnimalType.cs
+++ Anidopt/Models/AnimalType.cs
@@ -1,10 +1,13 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace Anidopt.Models;
 
+[PrimaryKey(nameof(Id))]
 public class AnimalType
 {
     public int Id { get; set; }
+
     [StringLength(32, MinimumLength = 1)]
     [Required]
     public string? Name { get; set; }
