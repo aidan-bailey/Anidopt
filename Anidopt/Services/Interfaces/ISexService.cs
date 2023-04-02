92fd96d36a8a4e86c0f784c59bb861294dccba66
Aidan Bailey <aidan.j.bailey@outlook.com>
2023-04-02 16:36:35 +0200

    Add Sex service

---
+++ Anidopt/Services/Interfaces/ISexService.cs
@@ -0,0 +1,12 @@
+﻿using Anidopt.Models;
+
+namespace Anidopt.Services.Interfaces;
+
+public interface ISexService
+{
+    public bool Initialised { get; }
+    public Task<List<Sex>> GetSexAsync();
+    public Task<Sex?> GetSexByIdAsync(int id);
+    public Task<bool> SexExistsByIdAsync(int id);
+    public Task EnsureSexDeletionByIdAsync(int id);
+}
---
+++ Anidopt/Services/SexService.cs
@@ -0,0 +1,31 @@
+﻿using Anidopt.Data;
+using Anidopt.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Anidopt.Services;
+
+public class SexService
+{
+    private readonly AnidoptContext _context;
+
+    public SexService(AnidoptContext context)
+    {
+        _context = context;
+    }
+
+    public bool Initialised => _context.Sex != null;
+
+    public async Task<List<Sex>> GetSexAsync() => await _context.Sex.ToListAsync();
+
+    public async Task<Sex?> GetSexByIdAsync(int id) => await _context.Sex.FindAsync(id);
+
+    public async Task<bool> SexExistsByIdAsync(int id) => await _context.Sex.AnyAsync(e => e.Id == id);
+
+    public async Task EnsureAnimalDeletionByIdAsync(int id)
+    {
+        var sex = await GetSexByIdAsync(id);
+        if (sex != null)
+            _context.Sex.Remove(sex);
+        await _context.SaveChangesAsync();
+    }
+}
