8ce6689887cfa8c30bad75498442d0fc0211ecf9
Aidan Bailey <aidan.j.bailey@outlook.com>
2023-04-02 11:34:15 +0200

    Add SpeciesBreedSelectUpder.js
    This function updates a Breed select based on a Species select

---
+++ Anidopt/wwwroot/js/SpeciesBreedSelectUpdater.js
@@ -0,0 +1,17 @@
+﻿$(function () {
+    $("#species-select").change(function () {
+        $.ajax({
+            type: "Get",
+            url: "/Breeds/ForSpecies?id=" + $(this).val(),
+            success: function (data) {
+                $("#breed-select").empty();
+                $.each(data, function (i, item) {
+                    $("#breed-select").append(new Option(item["name"], item["id"]));
+                });
+            },
+            error: function (response) {
+                console.log(response.responseText);
+            }
+        });
+    });
+});
