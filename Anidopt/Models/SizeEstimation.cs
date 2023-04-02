23edbcc1c2d85898354c75dc33f53ca9e2726baa
Aidan Bailey <aidan.j.bailey@outlook.com>
2023-04-02 17:05:11 +0200

    Add primary key decorator

--- Anidopt/Models/SizeEstimation.cs
+++ Anidopt/Models/SizeEstimation.cs
@@ -1,8 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Anidopt.Models;
 
+[PrimaryKey(nameof(Id))]
 public class SizeEstimation
 {
     /*********************
