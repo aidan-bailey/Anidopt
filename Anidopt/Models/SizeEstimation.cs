bd0df62ea09beb3a4e0b4ff8812a3fb14c423b32
Aidan Bailey <aidan.j.bailey@outlook.com>
2023-04-02 17:01:58 +0200

    Add SizeEstimation.cs

---
+++ Anidopt/Models/SizeEstimation.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Anidopt.Models;
+
+public class SizeEstimation
+{
+    public int Id { get; set; }
+
+    [Required]
+    public float Height { get; set; }
+
+    [Required]
+    public float Weight { get; set; }
+
+    [Required]
+    public int BreedId { get; set; }
+
+    [Required]
+    public int SexId { get; set; }
+}
