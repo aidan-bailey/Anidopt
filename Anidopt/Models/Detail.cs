4b4e4fc4f2bf229773186be4467e2c21f7d2fce7
Aidan Bailey <aidan.j.bailey@outlook.com>
2023-04-03 18:03:23 +0200

    Doc

--- Anidopt/Models/Detail.cs
+++ Anidopt/Models/Detail.cs
@@ -1,13 +1,30 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace Anidopt.Models;
 
+[PrimaryKey(nameof(Id))]
 public class Detail
 {
+    /*********************
+     * NATIVE PROPERTIES *
+     *********************/
+
     public int Id { get; set; }
+
     [Required]
     public string? Description { get; set; }
-    public virtual DescriptorLink? DescriptorLink { get; set; }
+
+    /****************
+     * FOREIGN KEYS *
+     ****************/
+
     [Required]
     public int DescriptorLinkId { get; set; }
+
+    /********************
+     * PROXY PROPERTIES *
+     ********************/
+
+    public virtual DescriptorLink? DescriptorLink { get; set; }
 }
