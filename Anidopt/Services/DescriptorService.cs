f7a9553e33c05d5ca500c3a174439bc5d876506a
Aidan Bailey <aidan.j.bailey@outlook.com>
2023-04-05 17:32:06 +0200

    Add Descriptor service

---
+++ Anidopt/Services/DescriptorService.cs
@@ -0,0 +1,31 @@
+﻿using Anidopt.Data;
+using Anidopt.Models;
+using Anidopt.Services.Interfaces;
+
+namespace Anidopt.Services;
+
+public class DescriptorService : IDescriptorService
+{
+    private readonly AnidoptContext _context;
+
+    public DescriptorService(AnidoptContext context)
+    {
+        _context = context;
+    }
+
+    public bool Initialised => _context.Descriptor != null;
+
+    public async Task<List<Descriptor>> GetDescriptorsAsync() => await _context.Descriptor.ToListAsync();
+
+    public async Task<Descriptor?> GetDescriptorByIdAsync(int id) => await _context.Descriptor.FindAsync(id);
+
+    public async Task<bool> DescriptorExistsByIdAsync(int id) => await _context.Descriptor.AnyAsync(e => e.Id == id);
+
+    public async Task EnsureDescriptorDeletionByIdAsync(int id)
+    {
+        var descriptor = await GetDescriptorByIdAsync(id);
+        if (descriptor != null)
+            _context.Descriptor.Remove(descriptor);
+        await _context.SaveChangesAsync();
+    }
+}
---
+++ Anidopt/Services/Interfaces/IDescriptorService.cs
@@ -0,0 +1,12 @@
+﻿using Anidopt.Models;
+
+namespace Anidopt.Services.Interfaces;
+
+public interface IDescriptorService
+{
+    public bool Initialised { get; }
+    public Task<List<Descriptor>> GetDescriptorsAsync();
+    public Task<Descriptor?> GetDescriptorByIdAsync(int id);
+    public Task<bool> DescriptorExistsByIdAsync(int id);
+    public Task EnsureDescriptorDeletionByIdAsync(int id);
+}
