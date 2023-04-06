9276b40369ee889ff2df43590a317210351ca29f
Aidan Bailey <aidan.j.bailey@outlook.com>
2023-04-07 00:05:26 +0200

    Add helper to make seeding more dry

--- Anidopt/Models/SeedData.cs
+++ Anidopt/Models/SeedData.cs
@@ -5,6 +5,15 @@ namespace Anidopt.Models;
 
 public static class SeedData
 {
+
+    private static void Seed<T>(this AnidoptContext context, params T[] entities) where T : class
+    {
+        var dbSet = context.Set<T>();
+        if (dbSet.Any()) return;
+        dbSet.AddRange(entities);
+        context.SaveChanges();
+    }
+
     public static void Initialize(IServiceProvider serviceProvider)
     {
         using (var context = new AnidoptContext(
@@ -15,196 +24,177 @@ public static class SeedData
             context.Database.EnsureDeleted(); // TODO - this is obviously bad!!!
             context.Database.EnsureCreated();
 
-            if (!context.Organisation.Any())
-            {
-                context.Organisation.AddRange(
-                    new Organisation
-                    {
-                        Name = "Mdzananda"
-                    }
-                );
-                context.SaveChanges();
-            }
+            // ORGANISATION
 
-            if (!context.Sex.Any())
-            {
-                context.Sex.AddRange(
-                    new Sex
-                    {
-                        Name = "Male"
-                    },
-                    new Sex
-                    {
-                        Name = "Female"
-                    }
-                );
-                context.SaveChanges();
-            }
+            context.Seed(
+                new Organisation
+                {
+                    Name = "Mdzananda"
+                }
+            );
 
-            if (!context.Species.Any())
-            {
-                context.Species.AddRange(
-                    new Species
-                    {
-                        Name = "Dog"
-                    },
-                    new Species
-                    {
-                        Name = "Cat"
-                    },
-                    new Species
-                    {
-                        Name = "Frog"
-                    }
-                );
-                context.SaveChanges();
-            }
+            // SEX
 
-            if (!context.Breed.Any())
-            {
-                context.Breed.AddRange(
-                    new Breed
-                    {
-                        Name = "Golden Retriever",
-                        Species = context.Species.Where(at => at.Name == "Dog").First()
-                    },
-                    new Breed
-                    {
-                        Name = "Afrikanis",
-                        Species = context.Species.Where(at => at.Name == "Dog").First()
-                    },
-                    new Breed
-                    {
-                        Name = "Siamese",
-                        Species = context.Species.Where(at => at.Name == "Cat").First()
-                    },
-                    new Breed
-                    {
-                        Name = "Red-Eyed Tree",
-                        Species = context.Species.Where(at => at.Name == "Frog").First()
-                    }
-                );
-                context.SaveChanges();
-            }
+            context.Seed(
+                new Sex
+                {
+                    Name = "Male"
+                },
+                new Sex
+                {
+                    Name = "Female"
+                }
+            );
 
-            // Look for any movies.
-            if (!context.Animal.Any())
-            {
-                context.Animal.AddRange(
-                    new Animal
-                    {
-                        Name = "Ginny",
-                        BirthDay = new DateTime(2016, 12, 16),
-                        Organisation = context.Organisation.Where(o => o.Name == "Mdzananda").First(),
-                        Breed = context.Breed.Where(b => b.Name == "Afrikanis").First(),
-                        Sex = context.Sex.Where(s => s.Name == "Female").First(),
-                        Description = "Ginny is a playful little pup who loves a good snooze.",
-                        Weight = 0,
-                        Height = 0
-                    },
-                    new Animal
-                    {
-                        Name = "Layla",
-                        BirthDay = new DateTime(2016, 12, 16),
-                        Organisation = context.Organisation.Where(o => o.Name == "Mdzananda").First(),
-                        Breed = context.Breed.Where(b => b.Name == "Afrikanis").First(),
-                        Sex = context.Sex.Where(s => s.Name == "Female").First(),
-                        Weight = 0,
-                        Height = 0
-                    }
-                );
-                context.SaveChanges();
-            }
+            // SPECIES
 
-            if (!context.DescriptorType.Any())
-            {
-                context.DescriptorType.AddRange(
-                    new DescriptorType
-                    {
-                        Name = "Social"
-                    },
-                    new DescriptorType
-                    {
-                        Name = "Medical"
-                    },
-                    new DescriptorType
-                    {
-                        Name = "Personal"
-                    }
-                );
-                context.SaveChanges();
-            }
+            context.Seed(
+                new Species
+                {
+                    Name = "Dog"
+                },
+                new Species
+                {
+                    Name = "Cat"
+                },
+                new Species
+                {
+                    Name = "Frog"
+                }
+            );
 
-            if (!context.Descriptor.Any())
-            {
-                context.Descriptor.AddRange(
-                    new Descriptor
-                    {
-                        Name = "Friendly with Cats",
-                        DescriptorType = context.DescriptorType.Where(at => at.Name == "Social").First()
-                    },
-                    new Descriptor
-                    {
-                        Name = "Nervous",
-                        DescriptorType = context.DescriptorType.Where(at => at.Name == "Personal").First()
-                    },
-                    new Descriptor
-                    {
-                        Name = "Dewormed",
-                        DescriptorType = context.DescriptorType.Where(at => at.Name == "Medical").First()
-                    },
-                    new Descriptor
-                    {
-                        Name = "Vaccinated",
-                        DescriptorType = context.DescriptorType.Where(at => at.Name == "Medical").First()
-                    }
-                );
-                context.SaveChanges();
-            }
+            // BREED
 
-            if (!context.DescriptorLink.Any())
-            {
-                context.DescriptorLink.AddRange(
-                    new DescriptorLink
-                    {
-                        Descriptor = context.Descriptor.Where(at => at.Name == "Dewormed").First(),
-                        Animal = context.Animal.Where(at => at.Name == "Ginny").First()
-                    },
-                    new DescriptorLink
-                    {
-                        Descriptor = context.Descriptor.Where(at => at.Name == "Nervous").First(),
-                        Animal = context.Animal.Where(at => at.Name == "Ginny").First()
-                    },
-                    new DescriptorLink
-                    {
-                        Descriptor = context.Descriptor.Where(at => at.Name == "Dewormed").First(),
-                        Animal = context.Animal.Where(at => at.Name == "Layla").First()
-                    }
-                );
-                context.SaveChanges();
-            }
+            context.Seed(
+                new Breed
+                {
+                    Name = "Golden Retriever",
+                    Species = context.Species.Where(at => at.Name == "Dog").First()
+                },
+                new Breed
+                {
+                    Name = "Afrikanis",
+                    Species = context.Species.Where(at => at.Name == "Dog").First()
+                },
+                new Breed
+                {
+                    Name = "Siamese",
+                    Species = context.Species.Where(at => at.Name == "Cat").First()
+                },
+                new Breed
+                {
+                    Name = "Red-Eyed Tree",
+                    Species = context.Species.Where(at => at.Name == "Frog").First()
+                }
+            );
 
-            if (!context.Detail.Any())
-            {
-                context.Detail.AddRange(
-                    new Detail
-                    {
-                        DescriptorLink = context.DescriptorLink.Where(at => at.Descriptor.Name == "Dewormed" && at.Animal.Name == "Ginny").First(),
-                        Description = "Happened on arrival at Mdzananda Animal Clinic"
-                    },
-                    new Detail
-                    {
-                        DescriptorLink = context.DescriptorLink.Where(at => at.Descriptor.Name == "Nervous" && at.Animal.Name == "Ginny").First(),
-                        Description = "Does not like going into the Laundry"
-                    },
-                    new Detail
-                    {
-                        DescriptorLink = context.DescriptorLink.Where(at => at.Descriptor.Name == "Nervous" && at.Animal.Name == "Ginny").First(),
-                        Description = "Does not like going into the Kitchen"
-                    }
-                );
-                context.SaveChanges();
-            }
+            // ANIMAL
+
+            context.Seed(
+                new Animal
+                {
+                    Name = "Ginny",
+                    BirthDay = new DateTime(2016, 12, 16),
+                    Organisation = context.Organisation.Where(o => o.Name == "Mdzananda").First(),
+                    Breed = context.Breed.Where(b => b.Name == "Afrikanis").First(),
+                    Sex = context.Sex.Where(s => s.Name == "Female").First(),
+                    Description = "Ginny is a playful little pup who loves a good snooze.",
+                    Weight = 0,
+                    Height = 0
+                },
+                new Animal
+                {
+                    Name = "Layla",
+                    BirthDay = new DateTime(2016, 12, 16),
+                    Organisation = context.Organisation.Where(o => o.Name == "Mdzananda").First(),
+                    Breed = context.Breed.Where(b => b.Name == "Afrikanis").First(),
+                    Sex = context.Sex.Where(s => s.Name == "Female").First(),
+                    Weight = 0,
+                    Height = 0
+                }
+            );
+
+            // DESCRIPTOR TYPE
+
+            context.Seed(
+                new DescriptorType
+                {
+                    Name = "Social"
+                },
+                new DescriptorType
+                {
+                    Name = "Medical"
+                },
+                new DescriptorType
+                {
+                    Name = "Personal"
+                }
+            );
+
+            // DESCRIPTOR
+
+            context.Seed(
+                new Descriptor
+                {
+                    Name = "Friendly with Cats",
+                    DescriptorType = context.DescriptorType.Where(at => at.Name == "Social").First()
+                },
+                new Descriptor
+                {
+                    Name = "Nervous",
+                    DescriptorType = context.DescriptorType.Where(at => at.Name == "Personal").First()
+                },
+                new Descriptor
+                {
+                    Name = "Dewormed",
+                    DescriptorType = context.DescriptorType.Where(at => at.Name == "Medical").First()
+                },
+                new Descriptor
+                {
+                    Name = "Vaccinated",
+                    DescriptorType = context.DescriptorType.Where(at => at.Name == "Medical").First()
+                }
+            );
+
+            // DESCRIPTOR LINK
+
+            context.Seed(
+                new DescriptorLink
+                {
+                    Descriptor = context.Descriptor.Where(at => at.Name == "Dewormed").First(),
+                    Animal = context.Animal.Where(at => at.Name == "Ginny").First()
+                },
+                new DescriptorLink
+                {
+                    Descriptor = context.Descriptor.Where(at => at.Name == "Nervous").First(),
+                    Animal = context.Animal.Where(at => at.Name == "Ginny").First()
+                },
+                new DescriptorLink
+                {
+                    Descriptor = context.Descriptor.Where(at => at.Name == "Dewormed").First(),
+                    Animal = context.Animal.Where(at => at.Name == "Layla").First()
+                }
+            );
+
+            // DETAIL
+
+            context.Seed(
+                new Detail
+                {
+                    DescriptorLink = context.DescriptorLink.Where(at => at.Descriptor.Name == "Dewormed" && at.Animal.Name == "Ginny").First(),
+                    Description = "Happened on arrival at Mdzananda Animal Clinic"
+                },
+                new Detail
+                {
+                    DescriptorLink = context.DescriptorLink.Where(at => at.Descriptor.Name == "Nervous" && at.Animal.Name == "Ginny").First(),
+                    Description = "Does not like going into the Laundry"
+                },
+                new Detail
+                {
+                    DescriptorLink = context.DescriptorLink.Where(at => at.Descriptor.Name == "Nervous" && at.Animal.Name == "Ginny").First(),
+                    Description = "Does not like going into the Kitchen"
+                }
+            );
 
         }
     }
