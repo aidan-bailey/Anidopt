6c39548f443a2e42929638651262501d26917225
Aidan Bailey <aidan.j.bailey@outlook.com>
2023-05-08 20:12:14 +0200

    Add Picture entity model

---
+++ Anidopt/Models/Picture.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Anidopt.Models;
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+public class Picture : EntityModelBase
+{
+    [Required]
+    public byte[] Image { get; set; } = new byte[] { };
+
+    [Required]
+    public int AnimalId { get; set; }
+
+    [Required]
+    public bool Showcase { get; set; }
+
+    [ForeignKey(nameof(AnimalId))]
+    public Animal Animal { get; set; }
+}
