6ff7f3f1d168fff3f4f166b993c261e12378d2d6
Aidan Bailey <aidan.j.bailey@outlook.com>
2023-05-08 20:20:37 +0200

    Add Picture scaffolding

---
+++ Anidopt/Controllers/PicturesController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Anidopt.Data;
+using Anidopt.Models;
+
+namespace Anidopt.Controllers
+{
+    public class PicturesController : Controller
+    {
+        private readonly AnidoptContext _context;
+
+        public PicturesController(AnidoptContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Pictures
+        public async Task<IActionResult> Index()
+        {
+            var anidoptContext = _context.Picture.Include(p => p.Animal);
+            return View(await anidoptContext.ToListAsync());
+        }
+
+        // GET: Pictures/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Picture == null)
+            {
+                return NotFound();
+            }
+
+            var picture = await _context.Picture
+                .Include(p => p.Animal)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (picture == null)
+            {
+                return NotFound();
+            }
+
+            return View(picture);
+        }
+
+        // GET: Pictures/Create
+        public IActionResult Create()
+        {
+            ViewData["AnimalId"] = new SelectList(_context.Animal, "Id", "Name");
+            return View();
+        }
+
+        // POST: Pictures/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Image,AnimalId,Showcase,Id")] Picture picture)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(picture);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AnimalId"] = new SelectList(_context.Animal, "Id", "Name", picture.AnimalId);
+            return View(picture);
+        }
+
+        // GET: Pictures/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Picture == null)
+            {
+                return NotFound();
+            }
+
+            var picture = await _context.Picture.FindAsync(id);
+            if (picture == null)
+            {
+                return NotFound();
+            }
+            ViewData["AnimalId"] = new SelectList(_context.Animal, "Id", "Name", picture.AnimalId);
+            return View(picture);
+        }
+
+        // POST: Pictures/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Image,AnimalId,Showcase,Id")] Picture picture)
+        {
+            if (id != picture.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(picture);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PictureExists(picture.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AnimalId"] = new SelectList(_context.Animal, "Id", "Name", picture.AnimalId);
+            return View(picture);
+        }
+
+        // GET: Pictures/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Picture == null)
+            {
+                return NotFound();
+            }
+
+            var picture = await _context.Picture
+                .Include(p => p.Animal)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (picture == null)
+            {
+                return NotFound();
+            }
+
+            return View(picture);
+        }
+
+        // POST: Pictures/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Picture == null)
+            {
+                return Problem("Entity set 'AnidoptContext.Picture'  is null.");
+            }
+            var picture = await _context.Picture.FindAsync(id);
+            if (picture != null)
+            {
+                _context.Picture.Remove(picture);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PictureExists(int id)
+        {
+          return (_context.Picture?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
