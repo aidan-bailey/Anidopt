44b5ef7896868cdb8153de4aeff9079f9bfac7bc
Aidan Bailey <aidan.j.bailey@outlook.com>
2023-05-22 17:39:05 +0200

    Cut regex from password

--- Anidopt/ViewModels/RegisterViewModel.cs
+++ Anidopt/ViewModels/RegisterViewModel.cs
@@ -15,7 +15,7 @@ public class RegisterViewModel
     [Required(ErrorMessage = "Please enter password")]
     [DataType(DataType.Password)]
     [StringLength(100, ErrorMessage = "Password \"{0}\" must have {2} character", MinimumLength = 8)]
-    [RegularExpression(@"^([a-zA-Z0-9@*#]{8,15})$", ErrorMessage = "Password must contain: Minimum 8 characters atleast 1 UpperCase Alphabet, 1 LowerCase      Alphabet, 1 Number and 1 Special Character")]
+    //[RegularExpression(@"^([a-zA-Z0-9@*#]{8,15})$", ErrorMessage = "Password must contain: Minimum 8 characters atleast 1 UpperCase Alphabet, 1 LowerCase      Alphabet, 1 Number and 1 Special Character")]
     public string Password { get; set; }
 
     [Display(Name = "Confirm password")]
