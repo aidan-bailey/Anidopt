5f58729458ebc94cde1bbb163665ae47ac3f44d8
Aidan Bailey <aidan.j.bailey@outlook.com>
2023-05-24 19:02:39 +0200

    Add UserOrganisationLink.cs

---
+++ Anidopt/Models/UserOrganisationLink.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Anidopt.Models;
+
+public class UserOrganisationLink : EntityModelBase
+{
+    [Required]
+    public int UserId { get; set; }
+
+    [ForeignKey(nameof(UserId))]
+    public virtual IdentityUser? User { get; set; }
+
+    [Required]
+    public int OrganisationId { get; set; }
+
+    [ForeignKey(nameof(OrganisationId))]
+    public virtual Organisation? Organisation { get; set; }
+
+    [Required]
+    public bool IsAdmin { get; set; }
+}
