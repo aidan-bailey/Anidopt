7cd72bc5f462ec5d3dde8ba29fd674b511599cf8
Aidan Bailey <aidan.j.bailey@outlook.com>
2023-05-24 19:14:41 +0200

    Add User.cs

---
+++ Anidopt/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Anidopt.Models;
+
+public class User : IdentityUser<int>
+{
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public virtual ICollection<UserOrganisationLink>? UserOrganisationLinks { get; set; }
+}
