b3f3c5ac646e19844c8e10ed95d1c3bba929667f
Aidan Bailey <aidan.j.bailey@outlook.com>
2023-05-24 19:16:06 +0200

    Add Required tag to FirstName and Surname

--- Anidopt/Models/User.cs
+++ Anidopt/Models/User.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace Anidopt.Models;
 
 public class User : IdentityUser<int>
 {
+    [Required]
     public string FirstName { get; set; } = null!;
 
+    [Required]
     public string LastName { get; set; } = null!;
 
     public virtual ICollection<UserOrganisationLink>? UserOrganisationLinks { get; set; }
