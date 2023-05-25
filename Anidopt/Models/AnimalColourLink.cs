5d246e012a8a087b492aea2e3f3ee28a0a29e05b
Aidan Bailey <aidan.j.bailey@outlook.com>
2023-05-25 18:41:20 +0200

    Add AnimalColourLink.cs

---
+++ Anidopt/Models/AnimalColourLink.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Anidopt.Models;
+
+public class AnimalColourLink
+{
+    [Required]
+    public string? AnimalId { get; set; }
+
+    [ForeignKey(nameof(AnimalId))]
+    public virtual Animal? Animal { get; set; }
+
+    [Required]
+    public int? ColourId { get; set; }
+
+    [ForeignKey(nameof(ColourId))]
+    public virtual AnimalColour? Colour { get; set; }
+}
