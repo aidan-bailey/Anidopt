42b174ea56285ef8eb74d8ab23386a4bb6f1b71b
Aidan Bailey <aidan.j.bailey@outlook.com>
2023-05-25 18:49:34 +0200

    Cleanup

--- Anidopt/Controllers/SiteAdminControllers/AnimalColoursController.cs
+++ Anidopt/Controllers/SiteAdminControllers/AnimalColoursController.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using Anidopt.Data;
+﻿using Anidopt.Data;
 using Anidopt.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Anidopt.Controllers.SiteAdminControllers
 {
