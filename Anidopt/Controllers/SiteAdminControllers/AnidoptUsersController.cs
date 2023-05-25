0a84856fec615d51301c3a45ea681e1a923e760b
Aidan Bailey <aidan.j.bailey@outlook.com>
2023-05-25 18:53:07 +0200

    Cleanup

--- Anidopt/Controllers/SiteAdminControllers/AnidoptUsersController.cs
+++ Anidopt/Controllers/SiteAdminControllers/AnidoptUsersController.cs
@@ -1,165 +1,159 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using Anidopt.Data;
+﻿using Anidopt.Data;
 using Anidopt.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
-namespace Anidopt.Controllers.SiteAdminControllers
+namespace Anidopt.Controllers.SiteAdminControllers;
+
+public class AnidoptUsersController : Controller
 {
-    public class AnidoptUsersController : Controller
+    private readonly AnidoptContext _context;
+
+    private string ViewPath(string name) => "~/Views/SiteAdmin/AnidoptUsers/" + name + ".cshtml";
+
+    public AnidoptUsersController(AnidoptContext context)
     {
-        private readonly AnidoptContext _context;
+        _context = context;
+    }
 
-        private string ViewPath(string name) => "~/Views/SiteAdmin/AnidoptUsers/" + name + ".cshtml";
+    // GET: Users
+    public async Task<IActionResult> Index()
+    {
+        return _context.AnidoptUser != null ?
+                    View(ViewPath("Index"), await _context.AnidoptUser.ToListAsync()) :
+                    Problem("Entity set 'AnidoptContext.User'  is null.");
+    }
 
-        public AnidoptUsersController(AnidoptContext context)
+    // GET: Users/Details/5
+    public async Task<IActionResult> Details(string? id)
+    {
+        if (id == null || _context.AnidoptUser == null)
         {
-            _context = context;
+            return NotFound();
         }
 
-        // GET: Users
-        public async Task<IActionResult> Index()
+        var user = await _context.AnidoptUser
+            .FirstOrDefaultAsync(m => m.Id == id);
+        if (user == null)
         {
-            return _context.AnidoptUser != null ?
-                        View(ViewPath("Index"), await _context.AnidoptUser.ToListAsync()) :
-                        Problem("Entity set 'AnidoptContext.User'  is null.");
+            return NotFound();
         }
 
-        // GET: Users/Details/5
-        public async Task<IActionResult> Details(string? id)
+        return View(ViewPath("Details"), user);
+    }
+
+    // GET: Users/Create
+    public IActionResult Create()
+    {
+        return View(ViewPath("Create"));
+    }
+
+    // POST: Users/Create
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("FirstName,LastName,Id,UserName,NormalizedUserName,Email,NormalizedEmail,EmailConfirmed,PasswordHash,SecurityStamp,ConcurrencyStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEnd,LockoutEnabled,AccessFailedCount")] AnidoptUser user)
+    {
+        if (ModelState.IsValid)
         {
-            if (id == null || _context.AnidoptUser == null)
-            {
-                return NotFound();
-            }
-
-            var user = await _context.AnidoptUser
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            return View(ViewPath("Details"), user);
-        }
-
-        // GET: Users/Create
-        public IActionResult Create()
-        {
-            return View(ViewPath("Create"));
-        }
-
-        // POST: Users/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("FirstName,LastName,Id,UserName,NormalizedUserName,Email,NormalizedEmail,EmailConfirmed,PasswordHash,SecurityStamp,ConcurrencyStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEnd,LockoutEnabled,AccessFailedCount")] AnidoptUser user)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(user);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(ViewPath("Create"), user);
-        }
-
-        // GET: Users/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null || _context.AnidoptUser == null)
-            {
-                return NotFound();
-            }
-
-            var user = await _context.AnidoptUser.FindAsync(id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-            return View(ViewPath("Edit"), user);
-        }
-
-        // POST: Users/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(string id, [Bind("FirstName,LastName,Id,UserName,NormalizedUserName,Email,NormalizedEmail,EmailConfirmed,PasswordHash,SecurityStamp,ConcurrencyStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEnd,LockoutEnabled,AccessFailedCount")] AnidoptUser user)
-        {
-            if (id != user.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(user);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!UserExists(user.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(ViewPath("Edit"), user);
-        }
-
-        // GET: Users/Delete/5
-        public async Task<IActionResult> Delete(string? id)
-        {
-            if (id == null || _context.AnidoptUser == null)
-            {
-                return NotFound();
-            }
-
-            var user = await _context.AnidoptUser
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            return View(ViewPath("Edit"), user);
-        }
-
-        // POST: Users/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(string id)
-        {
-            if (_context.AnidoptUser == null)
-            {
-                return Problem("Entity set 'AnidoptContext.User'  is null.");
-            }
-            var user = await _context.AnidoptUser.FindAsync(id);
-            if (user != null)
-            {
-                _context.AnidoptUser.Remove(user);
-            }
-
+            _context.Add(user);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
+        return View(ViewPath("Create"), user);
+    }
 
-        private bool UserExists(string id)
+    // GET: Users/Edit/5
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null || _context.AnidoptUser == null)
         {
-            return (_context.AnidoptUser?.Any(e => e.Id == id)).GetValueOrDefault();
+            return NotFound();
         }
+
+        var user = await _context.AnidoptUser.FindAsync(id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+        return View(ViewPath("Edit"), user);
+    }
+
+    // POST: Users/Edit/5
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(string id, [Bind("FirstName,LastName,Id,UserName,NormalizedUserName,Email,NormalizedEmail,EmailConfirmed,PasswordHash,SecurityStamp,ConcurrencyStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEnd,LockoutEnabled,AccessFailedCount")] AnidoptUser user)
+    {
+        if (id != user.Id)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                _context.Update(user);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(user.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        return View(ViewPath("Edit"), user);
+    }
+
+    // GET: Users/Delete/5
+    public async Task<IActionResult> Delete(string? id)
+    {
+        if (id == null || _context.AnidoptUser == null)
+        {
+            return NotFound();
+        }
+
+        var user = await _context.AnidoptUser
+            .FirstOrDefaultAsync(m => m.Id == id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        return View(ViewPath("Edit"), user);
+    }
+
+    // POST: Users/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(string id)
+    {
+        if (_context.AnidoptUser == null)
+        {
+            return Problem("Entity set 'AnidoptContext.User'  is null.");
+        }
+        var user = await _context.AnidoptUser.FindAsync(id);
+        if (user != null)
+        {
+            _context.AnidoptUser.Remove(user);
+        }
+
+        await _context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+    }
+
+    private bool UserExists(string id)
+    {
+        return (_context.AnidoptUser?.Any(e => e.Id == id)).GetValueOrDefault();
     }
 }
