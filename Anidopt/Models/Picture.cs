ee48547cb8cd1716f6fc4860d1f42a151905a8cb
Aidan Bailey <aidan.j.bailey@outlook.com>
2023-06-03 15:47:30 +0200

    Doc with regions

--- Anidopt/Models/Picture.cs
+++ Anidopt/Models/Picture.cs
@@ -5,15 +5,23 @@ namespace Anidopt.Models;
 
 public class Picture : EntityModelBase
 {
+    #region Native Properties
+    
     [Required]
     public byte[] Image { get; set; }
 
     [Required]
-    public int AnimalId { get; set; }
+    public bool Showcase { get; set; }
 
     [Required]
-    public bool Showcase { get; set; }
+    public int AnimalId { get; set; }
+    
+    #endregion
+
+    #region Proxy Properties
 
     [ForeignKey(nameof(AnimalId))]
     public virtual Animal? Animal { get; set; }
+    
+    #endregion
 }
