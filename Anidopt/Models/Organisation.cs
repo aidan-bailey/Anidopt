b112f8c5e15895bd889546d4b2b1067a622d5f89
Aidan Bailey <aidan.j.bailey@outlook.com>
2023-06-03 15:45:17 +0200

    Doc with regions

--- Anidopt/Models/Organisation.cs
+++ Anidopt/Models/Organisation.cs
@@ -6,8 +6,17 @@ namespace Anidopt.Models;
 
 public class Organisation : EntityModelBase
 {
+    #region Native Properties
+
     [Required]
     [StringLength(32, MinimumLength = 1)]
     public string Name { get; set; }
+
+    #endregion
+
+    #region Proxy Properties
+
     public virtual List<Animal>? Animals { get; set; }
+    
+    #endregion
 }
