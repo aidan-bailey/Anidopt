250b59b16de4e65de7ab2645d0f4dda63789fe67
Aidan Bailey <aidan.j.bailey@outlook.com>
2024-01-06 20:24:16 +0200

    Add ClassifiedViewModel.cs

---
+++ Anidopt/ViewModels/ClassifiedViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Anidopt.Models;
+
+namespace Anidopt.ViewModels;
+
+public class ClassifiedViewModel {
+    #region Animal Properties
+
+    [Required]
+    public string? Name { get; set; }
+
+    [Required]
+    public DateTime BirthDay { get; set; }
+
+    public string? Description { get; set; }
+
+    [Required]
+    public virtual float Weight { get; set; }
+
+    [Required]
+    public int BreedId { get; set; }
+
+    [Required]
+    public int SexId { get; set; }
+
+    [Required]
+    public int SizeId { get; set; }
+
+    [ForeignKey(nameof(SizeId))]
+    public virtual Size? Size { get; set; }
+
+    [ForeignKey(nameof(SexId))]
+    public virtual Sex? Sex { get; set; }
+
+    [ForeignKey(nameof(BreedId))]
+    public virtual Breed? Breed { get; set; }
+
+    public virtual List<DescriptorLink>? DescriptorLinks { get; set; } = new();
+
+    public virtual List<AnimalColourLink>? AnimalColourLinks { get; set; } = new();
+
+    public virtual List<Picture> Pictures { get; set; } = new();
+
+    #endregion
+}
